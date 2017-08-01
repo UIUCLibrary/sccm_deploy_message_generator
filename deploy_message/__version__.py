a83a9e0d1e277fb66861c0f8e5279c7ccd118b6f
hborcher <hborcher@illinois.edu>
2017-08-01 10:01:08 -0500

    init commit

---
+++ deploy_message/__version__.py
@@ -0,0 +1,10 @@
+__title__ = 'deploy_message'
+__version__ = '0.0.2'
+__url__ = 'https://github.com/UIUCLibrary/sccm_deploy_message_generator'
+__author__ = 'University of Illinois at Urbana Champaign'
+__author_email__ = 'hborcher@illinois.edu'
+__description__ = 'Generates the message required for deployment'
+maintainer = "Henry Borchers"
+maintainer_email = "hborcher@illinois.edu"
+download_url = "https://github.com/UIUCLibrary/sccm_deploy_message_generator"
+license_ = "University of Illinois/NCSA Open Source License"
