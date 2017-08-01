8336f2e13ed6e7b2cf804f03a91a9e21f38f2301
hborcher <hborcher@illinois.edu>
2017-08-01 10:35:41 -0500

    added generate body

--- deploy_message/__init__.py
+++ deploy_message/__init__.py
@@ -1,3 +1,4 @@
-from .generate import generate_message, MessageData, get_footer
+from .generate import MessageData, get_footer, generate_body
 from .__version__ import __title__, __version__, __url__, __author__, __author_email__, __description__
 from .__version__ import maintainer, maintainer_email, download_url, license_
+from .generate import generate_message
