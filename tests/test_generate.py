55ed04874a01393821115f0b280119e3f5c575a6
hborcher <hborcher@illinois.edu>
2017-08-01 10:37:27 -0500

    import the entire deploy_message package.  Created test for generate_body()

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -1,12 +1,12 @@
 import pytest
-from deploy_message import generate_message, MessageData
-from deploy_message import generate
+import deploy_message
 
 expected_message = r"""Dear Scott,
 
 A new install package is ready for SCCM deployment.
 
 Filename: DS Hathi Trust Validate-amd64.msi
+
 Location: \\files.library.illinois.edu\groupfiles\Digital Content Creation\SCCM Upload\
 
 
@@ -26,6 +26,10 @@ This is an automated message but if you have questions, please contact Henry Bor
 Thank you for your time.
 """
 
+expected_footer = r"""Message generated using deploy_message version {}.
+(https://github.com/UIUCLibrary/sccm_deploy_message_generator)
+""".format(deploy_message.__version__)
+
 yml_data = """deployer:
   name: Scott
 package:
@@ -44,6 +48,8 @@ deployment:
     - LIBSTFDCC09
     - LIBSTFDCC013
 """
+
+
 @pytest.fixture(scope="session")
 def yml_file(tmpdir_factory):
     tmp_yml_file = str(tmpdir_factory.mktemp("sample").join("sample.yml"))
@@ -53,7 +59,7 @@ def yml_file(tmpdir_factory):
 
 
 def test_generate_message():
-    message = MessageData()
+    message = deploy_message.MessageData()
     message.receiver_name = "Scott"
     message.package_filename = "DS Hathi Trust Validate-amd64.msi"
     message.package_path = "\\\\files.library.illinois.edu\groupfiles\Digital Content Creation\SCCM Upload\\"
@@ -66,16 +72,41 @@ def test_generate_message():
                                     "LIBSTFDCC08 (413)",
                                     "LIBSTFDCC09 (lab)",
                                     "LIBSTFDCC013 (lab)"]
-    for generated_line, expected_line in zip(generate_message(message).split("\n"), expected_message.split("\n")):
-        assert generated_line == expected_line
+    for i, (generated_line, expected_line) in enumerate(
+            zip(deploy_message.generate_message(message).split("\n"), expected_message.split("\n"))):
+        assert generated_line == expected_line, "differences on line {}".format(i + 1)
+
+
+def test_generate_body():
+    message = deploy_message.MessageData()
+    message.receiver_name = "Scott"
+    message.package_filename = "DS Hathi Trust Validate-amd64.msi"
+    message.package_path = "\\\\files.library.illinois.edu\groupfiles\Digital Content Creation\SCCM Upload\\"
+    message.maintainer_name = "Henry Borchers"
+    message.maintainer_email = "hborcher@illinois.edu"
+    message.deployment_hostnames = ["LIBSTFDCC02 (413)",
+                                    "LIBSTFDCC03 (413)",
+                                    "LIBSTFDCC04 (413)",
+                                    "LIBSTFDCC06 (413)",
+                                    "LIBSTFDCC08 (413)",
+                                    "LIBSTFDCC09 (lab)",
+                                    "LIBSTFDCC013 (lab)"]
+    for i, (generated_line, expected_line) in enumerate(
+            zip(deploy_message.generate_body(message).split("\n"), expected_message.split("\n"))):
+        assert generated_line == expected_line, "differences on line {}".format(i + 1)
+
+
+def test_generate_footer():
+    footer = deploy_message.get_footer()
+    assert footer == expected_footer
 
 
 def test_get_yml(yml_file):
     print(type(yml_file))
     with open(yml_file) as f:
         print(f.read())
-    data = generate.get_message_data(yml_file)
-    assert isinstance(data, MessageData)
+    data = deploy_message.generate.get_message_data(yml_file)
+    assert isinstance(data, deploy_message.MessageData)
     assert data.receiver_name == "Scott"
     assert data.package_filename == "DS Hathi Trust Validate-amd64.msi"
     assert data.package_path == "\\\\files.library.illinois.edu\groupfiles\Digital Content Creation\SCCM Upload\\"
